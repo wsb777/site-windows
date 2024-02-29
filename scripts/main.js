35982f9313f7235630496aacd06cba780684ea00
wsb777 <a.sizov.wsb@gmail.com>
2024-02-29 23:53:17 +0300

    empty-windows

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function date() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let timeText = hours + ":" + minutes;
+    let day = now.getDate();
+    let month = now.getMonth() + 1;
+    month = '' + month;
+    if (month.length == 1) {
+        month = "0" + month;
+    }
+    let year = now.getFullYear();
+    let dateText = day + "." + month + "." + year;
+    let time = document.querySelector('.time');
+    let date = document.querySelector('.date');
+    time.textContent = timeText;
+    date.textContent = dateText;
+}
+date();
+setInterval(function(){
+    date()
+}, 30000);
