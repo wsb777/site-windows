4d373a865dc7bfa40c127ce4aef171d25e8dc44d
wsb777 <a.sizov.wsb@gmail.com>
2024-03-02 01:53:13 +0300

    click fixs

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,54 @@ date();
 setInterval(function(){
     date()
 }, 10000);
+
+const desktop = document.querySelector('main');
+const textFile = document.querySelector('.text-file');
+const siteFolder = document.querySelector('.sites');
+const stackFolder = document.querySelector('.stack');
+const contactsFolder = document.querySelector('.contacts');
+
+function activeTest() {
+    let folders = document.querySelectorAll('.folder');
+    for (i=0; i < folders.length; i++) {
+        if(folders[i].classList.contains('active')) {
+            folders[i].classList.remove('active');
+          }
+    }
+}
+function textFileOpen() {
+    activeTest();
+    if (!textFile.classList.contains('active')) {
+    textFile.classList.add('active');
+    }
+}
+
+function siteFolderOpen() {
+    activeTest();
+    if (!siteFolder.classList.contains('active')) {
+    siteFolder.classList.add('active');
+    }
+}
+
+function stackFolderOpen() {
+    activeTest();
+    if (!stackFolder.classList.contains('active')) {
+    stackFolder.classList.add('active');
+    }
+}
+
+function contactsFolderOpen() {
+    activeTest();
+    if (!contactsFolder.classList.contains('active')) {
+    contactsFolder.classList.add('active');
+    }
+}
+
+function click() {
+    console.log('попал');
+}
+// desktop.addEventListener('click', click);
+textFile.addEventListener('click',textFileOpen);
+siteFolder.addEventListener('click',siteFolderOpen);
+stackFolder.addEventListener('click',stackFolderOpen);
+contactsFolder.addEventListener('click',contactsFolderOpen);
